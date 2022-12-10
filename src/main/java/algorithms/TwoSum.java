1f6b0f7023e522c30b035a9d8ffa19c714fcbfdd
Andrzej Szczap <andrzejandrejoo@gmail.com>
2022-12-10 03:03:59 +0100

    Added twoSum algorithm, implemented the tests.

---
+++ src/main/java/algorithms/TwoSum.java
@@ -0,0 +1,24 @@
+package algorithms;
+
+import java.util.HashMap;
+
+/**
+ * @link <a href="https://leetcode.com/problems/two-sum/">https://leetcode.com/problems/two-sum/</a>
+ * @date 10.12.2022
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int rest = target - nums[i];
+
+            if (hashMap.containsKey(rest)) {
+                return new int[]{hashMap.get(rest), i};
+            } else {
+                hashMap.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+}
---
+++ src/test/java/algorithms/TwoSumTest.java
@@ -0,0 +1,42 @@
+package algorithms;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TwoSumTest {
+
+    private static TwoSum twoSum;
+
+    @BeforeEach
+    void setUp() {
+        twoSum = new TwoSum();
+    }
+
+
+    @Test
+    void should_pass_the_test() {
+
+        // test1
+        int[] array1 = new int[]{2, 7, 11, 15};
+        int target1 = 9;
+        int[] expected1 = new int[]{0, 1};
+
+        // test2
+        int[] array2 = new int[]{3, 2, 4};
+        int target2 = 6;
+        int[] expected2 = new int[]{1, 2};
+
+        // test3
+        int[] array3 = new int[]{3, 3};
+        int target3 = 6;
+        int[] expected3 = new int[]{0, 1};
+
+        assertThat(twoSum.twoSum(array1, target1)).isEqualTo(expected1);
+        assertThat(twoSum.twoSum(array2, target2)).isEqualTo(expected2);
+        assertThat(twoSum.twoSum(array3, target3)).isEqualTo(expected3);
+
+    }
+}
