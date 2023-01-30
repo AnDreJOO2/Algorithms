cc4a53eaefa415d4a5a4cdd5694cfda95985c2e6
Andrzej Szczap <andrzejandrejoo@gmail.com>
2023-01-30 14:28:58 +0100

    Added ReverseLinkedList algorithm.

---
+++ src/main/java/algorithms/leetcode/ReverseLinkedList.java
@@ -0,0 +1,69 @@
+package algorithms.leetcode;
+
+public class ReverseLinkedList {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode previous = null;
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = previous;
+            previous = head;
+            head = next;
+        }
+        return previous;
+
+    }
+}
+
+//Example:
+/*
+1st iteration
+previous=null
+head =1
+next = 2
+head.next = null
+previous = 1
+head = 2
+
+2nd iteration
+head=2
+next = 3
+head.next = 1
+previous = 2
+head = 3
+
+...
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
