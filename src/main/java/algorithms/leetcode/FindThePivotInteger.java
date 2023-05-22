c74f1aa79b25803e98538f636fdfccec0d6d14aa
Andrzej Szczap <andrzejandrejoo@gmail.com>
2023-05-22 23:49:12 +0200

    date change

--- src/main/java/algorithms/leetcode/FindThePivotInteger.java
+++ src/main/java/algorithms/leetcode/FindThePivotInteger.java
@@ -2,7 +2,7 @@ package algorithms.leetcode;
 
 /**
  * @link <a href="https://leetcode.com/problems/find-the-pivot-integer/">https://leetcode.com/problems/find-the-pivot-integer/</a>
- * @date 21.05.2023
+ * @date 22.05.2023
  */
 public class FindThePivotInteger {
     public int pivotInteger(int n) {
